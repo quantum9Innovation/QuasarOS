e64ec3dd85582d3e247f0edeb89e5b894f066531
Ananth Venkatesh <dev.quantum9innovation@gmail.com>
2024-08-31 15:49:22 -0400

    style(nixfmt): flake.nix

--- flake.nix
+++ flake.nix
@@ -61,8 +61,8 @@
     make = { hostname, user, name, git, hardware, system ? "x86_64-linux"
       , kernel ? "zen", secureboot ? { enabled = true; }, stateVersion ? "24.05"
       , systemPackages, homePackages, autoLogin ? true
-      , ssh ? { enabled = false; }
-      , locale ? "en_US.UTF-8", hyprland ? { mod = "SUPER"; }, graphics ? {
+      , ssh ? { enabled = false; }, locale ? "en_US.UTF-8"
+      , hyprland ? { mod = "SUPER"; }, graphics ? {
         opengl = true;
         nvidia = {
           enabled = true;
