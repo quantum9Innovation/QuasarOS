5e37f02cb4d930dcfe2413ecb0e4a5b9789a6a2a
Ananth Venkatesh <dev.quantum9innovation@gmail.com>
2024-09-27 02:39:25 -0400

    fix(sec): disable highly insecure CUPS printing server

--- configuration.nix
+++ configuration.nix
@@ -123,7 +123,7 @@
   };
 
   # Enable CUPS to print documents
-  services.printing.enable = true;
+  services.printing.enable = false;
 
   # Enable sound with pipewire
   hardware.pulseaudio.enable = false;
