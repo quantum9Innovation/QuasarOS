ef298871e3b8f6e05185a73ea81ac041c621bacc
Ananth Venkatesh <dev.quantum9innovation@gmail.com>
2024-10-26 22:21:14 -0400

    feat(pkg): add tailscale

--- configuration.nix
+++ configuration.nix
@@ -125,6 +125,9 @@
   # Enable CUPS to print documents
   services.printing.enable = false;
 
+  # Tailscale?
+  services.tailscale.enable = true;
+
   # Enable sound with pipewire
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
