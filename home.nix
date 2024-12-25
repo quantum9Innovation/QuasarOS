quantum9Innovation/QuasarOS

commit 4f2e3f3c7503d0b381105d0ddc58ec392d8698be

feat: test setting default browser

home.nix
@@ -341,4 +341,16 @@ quasar: hyprPlugins: pack:
 
   home.stateVersion = quasar.stateVersion;
   programs.home-manager.enable = true;
+
+  xdg.mimeApps = {
+    enable = true;
+
+    defaultApplications = {
+      "text/html" = "userapp-Zen Browser-092UX2.desktop";
+      "x-scheme-handler/http" = "userapp-Zen Browser-092UX2.desktop";
+      "x-scheme-handler/https" = "userapp-Zen Browser-092UX2.desktop";
+      "x-scheme-handler/about" = "userapp-Zen Browser-092UX2.desktop";
+      "x-scheme-handler/unknown" = "userapp-Zen Browser-092UX2.desktop";
+    };
+  };
 }

commit bd089a9bb66d61e94dd2e5e94c3a9cd7abc31519

fix: hopefully avert critical home-manager failure
probably due to invalid default app settings introduced in 4f2e3f3c7503d0b381105d0ddc58ec392d8698be

home.nix
@@ -341,16 +341,4 @@ quasar: hyprPlugins: pack:
 
   home.stateVersion = quasar.stateVersion;
   programs.home-manager.enable = true;
-
-  xdg.mimeApps = {
-    enable = true;
-
-    defaultApplications = {
-      "text/html" = "userapp-Zen Browser-092UX2.desktop";
-      "x-scheme-handler/http" = "userapp-Zen Browser-092UX2.desktop";
-      "x-scheme-handler/https" = "userapp-Zen Browser-092UX2.desktop";
-      "x-scheme-handler/about" = "userapp-Zen Browser-092UX2.desktop";
-      "x-scheme-handler/unknown" = "userapp-Zen Browser-092UX2.desktop";
-    };
-  };
 }