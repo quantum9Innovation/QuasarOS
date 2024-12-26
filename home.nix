736c316cdbd0c55f3121a09357aa99073206a16a
Ananth Venkatesh <dev.quantum9innovation@gmail.com>
2024-12-26 17:22:54 -0500

    fix: critical home-manager error

--- home.nix
+++ home.nix
@@ -341,11 +341,11 @@ quasar: hyprPlugins: pack:
     enable = true;
 
     defaultApplications = {
-      "text/html" = "zen.desktop";
-      "x-scheme-handler/http" = "zen.desktop";
-      "x-scheme-handler/https" = "zen.desktop";
-      "x-scheme-handler/about" = "zen.desktop";
-      "x-scheme-handler/unknown" = "zen.desktop";
+      "text/html" = [ "zen.desktop" ];
+      "x-scheme-handler/http" = [ "zen.desktop" ];
+      "x-scheme-handler/https" = [ "zen.desktop" ];
+      "x-scheme-handler/about" = [ "zen.desktop" ];
+      "x-scheme-handler/unknown" = [ "zen.desktop" ];
     };
   };
 
