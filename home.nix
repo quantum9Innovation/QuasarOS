ed1ff6242aa46c44cec26d02b07daa621dea5a58
Ananth Venkatesh <dev.quantum9innovation@gmail.com>
2024-12-26 17:18:00 -0500

    feat: set default browser
    hopefully correctly this time

--- home.nix
+++ home.nix
@@ -337,6 +337,18 @@ quasar: hyprPlugins: pack:
     };
   };
 
+  xdg.mimeApps = {
+    enable = true;
+
+    defaultApplications = {
+      "text/html" = "zen.desktop";
+      "x-scheme-handler/http" = "zen.desktop";
+      "x-scheme-handler/https" = "zen.desktop";
+      "x-scheme-handler/about" = "zen.desktop";
+      "x-scheme-handler/unknown" = "zen.desktop";
+    };
+  };
+
   programs.chromium.commandLineArgs = "--enable-features=UseOzonePlatform --ozone-platform=wayland";
 
   home.stateVersion = quasar.stateVersion;
