095415b0c014f5449fb7b85400859f216b17bec0
Ananth Venkatesh <dev.quantum9innovation@gmail.com>
2024-12-28 01:10:01 -0500

    refactor: modularize secure boot

--- flake.nix
+++ flake.nix
@@ -125,27 +125,7 @@
           # Secure boot configuration
           secureBoot = [
             lanzaboote.nixosModules.lanzaboote
-
-            (
-              { pkgs, lib, ... }:
-              {
-                environment.systemPackages = [
-                  # For debugging and troubleshooting secure boot
-                  pkgs.sbctl
-                ];
-
-                # Lanzaboote currently replaces the systemd-boot module.
-                # This setting is usually set to true in configuration.nix,
-                # generated at installation time.
-                # So we force it to false for now.
-                boot.loader.systemd-boot.enable = lib.mkForce false;
-
-                boot.lanzaboote = {
-                  enable = true;
-                  pkiBundle = "/etc/secureboot";
-                };
-              }
-            )
+            (import ./modules/lanzaboote.nix)
           ];
 
           # Modules without conditional add-ons
---
+++ modules/lanzaboote.nix
@@ -0,0 +1,32 @@
+{ pkgs, lib, ... }:
+{
+
+  #  /*****                                                 /******   /****
+  #  |*    |  |*   |    **     ****     **    *****        |*    |  /*    *
+  #  |*    |  |*   |   /* *   /*       /* *   |*   |      |*    |  |*
+  #  |*    |  |*   |  /*   *   ****   /*   *  |*   /     |*    |   ******
+  #  |*  * |  |*   |  ******       |  ******  *****     |*    |         |
+  #  |*   *   |*   |  |*   |   *   |  |*   |  |*  *    |*    |   *     |
+  #   **** *   ****   |*   |    ****  |*   |  |*   *   ******    *****
+  #
+  #  ==========================================================================
+
+  # This module sets up secure boot infrastructure using Lanzaboote.
+  # It is loaded into the main system configuration if secure boot is enabled.
+
+  environment.systemPackages = [
+    # For debugging and troubleshooting secure boot
+    pkgs.sbctl
+  ];
+
+  # Lanzaboote currently replaces the systemd-boot module.
+  # This setting is usually set to true in configuration.nix,
+  # generated at installation time.
+  # So we force it to false for now.
+  boot.loader.systemd-boot.enable = lib.mkForce false;
+
+  boot.lanzaboote = {
+    enable = true;
+    pkiBundle = "/etc/secureboot";
+  };
+}
