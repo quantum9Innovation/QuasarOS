7dbe62fad0bb509c2e860d4bab0ea773d98aa952
Ananth Venkatesh <dev.quantum9innovation@gmail.com>
2025-01-04 22:52:18 -0800

    feat: add documentation for stylix

--- home.nix
+++ home.nix
@@ -374,4 +374,84 @@ quasar: hyprPlugins: pack:
 
   home.stateVersion = quasar.stateVersion;
   programs.home-manager.enable = true;
+
+  services.hyprpaper.enable = true;
+
+  programs.hyprlock = {
+    enable = true;
+    settings = {
+      general = {
+        hide_cursor = true;
+        grace = 1;
+      };
+      background = {
+        monitor = "";
+        path = "/tmp/__hyprlock-monitor-screenshot.png";
+        blur_passes = 3;
+        blur_size = 7;
+        noise = 0.0117;
+        contrast = 0.8916;
+        brightness = 0.8172;
+        vibrancy = 0.1696;
+        vibrancy_darkness = 0.0;
+      };
+      input-field = {
+        monitor = "";
+        size = "200, 50";
+        outline_thickness = 3;
+        dots_size = 0.33;
+        dots_spacing = 0.15;
+        dots_center = false;
+        dots_rounding = -1;
+        outer_color = "rgb(151515)";
+        inner_color = "rgb(200, 200, 200)";
+        font_color = "rgb(10, 10, 10)";
+        fade_on_empty = true;
+        fade_timeout = 1000;
+        placeholder_text = "<i>Input Password...</i>";
+        hide_input = false;
+        rounding = -1;
+        check_color = "rgb(204, 136, 34)";
+        fail_color = "rgb(204, 34, 34)";
+        fail_text = "<i>$FAIL <b>($ATTEMPTS)</b></i>";
+        fail_timeout = 2000;
+        fail_transition = 300;
+        capslock_color = -1;
+        numlock_color = -1;
+        bothlock_color = -1;
+        invert_numlock = false;
+        swap_font_color = false;
+
+        position = "0, -20";
+        halign = "center";
+        valign = "center";
+      };
+    };
+  };
+
+  services.hypridle = {
+    enable = true;
+    settings = {
+      general = {
+        lock_cmd = "pidof hyprlock || ${pkgs.grim}/bin/grim -o ${config.programs.hyprlock.settings.background.monitor} /tmp/__hyprlock-monitor-screenshot.png && ${pkgs.hyprlock}/bin/hyprlock"; # avoid starting multiple hyprlock instances.
+        before_sleep_cmd = "loginctl lock-session"; # lock before suspend.
+        after_sleep_cmd = "hyprctl dispatch dpms on"; # to avoid having to press a key twice to turn on the display.
+      };
+      listener = [
+        {
+          timeout = 1500;
+          on-timeout = "loginctl lock-session";
+        }
+        {
+          timeout = 330; # 5.5min
+          on-timeout = "hyprctl dispatch dpms off"; # screen off when timeout has passed
+          on-resume = "hyprctl dispatch dpms on"; # screen on when activity is detected after timeout has fired.
+        }
+        {
+          timeout = 1800;
+          on-timeout = "systemctl suspend";
+        }
+      ];
+    };
+  };
 }
