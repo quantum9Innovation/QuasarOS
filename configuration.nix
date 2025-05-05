9e1cf157f4f1438e20a0ef23ba908239095cd230
Ananth Venkatesh <dev.quantum9innovation@gmail.com>
2025-05-05 03:22:42 -0400

    feat: enable nix default auto updater

--- configuration.nix
+++ configuration.nix
@@ -376,14 +376,27 @@
   # Enable Docker for hardware virtualization
   virtualisation.docker.enable = true;
 
-  # This value determines the NixOS release from which the default settings
-  # for stateful data, like file locations and database versions
-  # on your system were taken.
-  # It‘s perfectly fine and recommended to leave this value at the release
-  # version of the first install of this system.
-  # Before changing this value, read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = quasar.stateVersion; # Did you read the comment?
+  # Core system settings
+  system = {
+    # This value determines the NixOS release from which the default settings
+    # for stateful data, like file locations and database versions
+    # on your system were taken.
+    # It‘s perfectly fine and recommended to leave this value at the release
+    # version of the first install of this system.
+    # Before changing this value, read the documentation for this option
+    # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+    inherit (quasar) stateVersion; # Did you read the comment?
+
+    # Automatically rebuild system daily
+    autoUpgrade = {
+      enable = true;
+      inherit (quasar) flake;
+      flags = [
+        "-L" # print build logs
+      ];
+      dates = "daily";
+    };
+  };
 
   # Stylix autoricer
   stylix = {
