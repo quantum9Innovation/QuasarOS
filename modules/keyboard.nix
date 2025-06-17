812c55acc42d7825de3878bdb1a6c4deb368cbb6
Ananth Venkatesh <dev.quantum9innovation@gmail.com>
2025-06-17 14:42:30 -0700

    fix: add the entire keyboard

--- modules/keyboard.nix
+++ modules/keyboard.nix
@@ -8,12 +8,24 @@ quasar: {
     )
 
     (defsrc
-      esc
-      caps
+      esc  f1  f2  f3  f4  f5  f6  f7  f8  f9  f10 f11 f12
+      prtsc scroll pause insert home pgup delete end pgdn
+      `  1  2  3  4  5  6  7  8  9  0  -  =  bspc
+      tab q  w  e  r  t  y  u  i  o  p  [  ]  \
+      caps a  s  d  f  g  h  j  k  l  ;  '  enter
+      lshift z  x  c  v  b  n  m  ,  .  /  rshift
+      lctrl lmeta lalt space ralt rmeta menu rctrl
+      up down left right
     )
 
     (deflayer qwerty
-      caps  esc
-      esc   caps
+      caps f1  f2  f3  f4  f5  f6  f7  f8  f9  f10 f11 f12
+      prtsc scroll pause insert home pgup delete end pgdn
+      `  1  2  3  4  5  6  7  8  9  0  -  =  bspc
+      tab q  w  e  r  t  y  u  i  o  p  [  ]  \
+      esc a  s  d  f  g  h  j  k  l  ;  '  enter
+      lshift z  x  c  v  b  n  m  ,  .  /  rshift
+      lctrl lmeta lalt space ralt rmeta menu rctrl
+      up down left right
     )'';
 }
