23d96d63e7d2a203b278fdf2ba86fd9632281fff
Ananth Venkatesh <dev.quantum9innovation@gmail.com>
2025-06-17 19:05:05 -0700

    fix: use correct keyboard source

--- modules/keyboard.nix
+++ modules/keyboard.nix
@@ -8,24 +8,22 @@ quasar: {
     )
 
     (defsrc
-      esc  f1  f2  f3  f4  f5  f6  f7  f8  f9  f10 f11 f12
-      print scroll pause insert home pgup delete end pgdn
-      `  1  2  3  4  5  6  7  8  9  0  -  =  bspc
-      tab q  w  e  r  t  y  u  i  o  p  [  ]  \
-      caps a  s  d  f  g  h  j  k  l  ;  '  enter
-      lshift z  x  c  v  b  n  m  ,  .  /  rshift
-      lctrl lmeta lalt space ralt rmeta menu rctrl
-      up down left right
+      esc  f1   f2   f3   f4   f5   f6   f7   f8   f9   f10  f11   f12 home end  del
+      grv  1    2    3    4    5    6    7    8    9    0    -     =        bspc
+      tab  q    w    e    r    t    y    u    i    o    p    [     ]        ret
+      caps a    s    d    f    g    h    j    k    l    ;    '     `        ret
+      lsft \    z    x    c    v    b    n    m    ,    .    /              rsft
+           lctl lmet lalt           spc            ralt prnt rctl           up
+                                                                       left down rght
     )
 
     (deflayer qwerty
-      caps f1  f2  f3  f4  f5  f6  f7  f8  f9  f10 f11 f12
-      print scroll pause insert home pgup delete end pgdn
-      `  1  2  3  4  5  6  7  8  9  0  -  =  bspc
-      tab q  w  e  r  t  y  u  i  o  p  [  ]  \
-      esc a  s  d  f  g  h  j  k  l  ;  '  enter
-      lshift z  x  c  v  b  n  m  ,  .  /  rshift
-      lctrl lmeta lalt space ralt rmeta menu rctrl
-      up down left right
+      caps  f1   f2   f3   f4   f5   f6   f7   f8   f9   f10  f11   f12 home end  del
+      grv  1    2    3    4    5    6    7    8    9    0    -     =        bspc
+      tab  q    w    e    r    t    y    u    i    o    p    [     ]        ret
+      esc a    s    d    f    g    h    j    k    l    ;    '     `         ret
+      lsft \    z    x    c    v    b    n    m    ,    .    /              rsft
+           lctl lmet lalt           spc            ralt prnt rctl           up
+                                                                       left down rght
     )'';
 }
