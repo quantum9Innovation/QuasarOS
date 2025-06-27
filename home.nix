1a7c62f01aae726fc66d0c19a7970b27dc6fe506
Ananth Venkatesh <dev.quantum9innovation@gmail.com>
2025-06-26 22:56:54 -0700

    feat(git): add credential helper

--- home.nix
+++ home.nix
@@ -266,6 +266,7 @@ quasar: utils: _upstream: hyprscroller-src: pack:
       delta.enable = true;
       extraConfig = {
         init.defaultBranch = "main";
+        credential.helper = "${pkgs.git.override { withLibsecret = true; }}/bin/git-credential-libsecret";
       };
     };
 
