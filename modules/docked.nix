fc99241468f319596575579fc55e1b00991ca8ae
Ananth Venkatesh <dev.quantum9innovation@gmail.com>
2025-07-20 15:34:24 -0700

    fix: use one-liner sleep management script

--- modules/docked.nix
+++ modules/docked.nix
@@ -1,12 +1,3 @@
 wlr-randr: screen_off: sleep: {
-  script = ''
-    hdmi_present=$(${wlr-randr} | grep -q "HDMI" && echo true || echo false)
-    if [ "$hdmi_present" = "true" ]; then
-      # HDMI is connected, perform actions for docked mode (only turn off screen)
-      ${screen_off}
-    else
-      # HDMI is not connected, perform actions for undocked mode
-      ${sleep}
-    fi
-  '';
+  script = ''hdmi_present=$(${wlr-randr} | grep -q "HDMI" && echo true || echo false); if [ "$hdmi_present" = "true" ]; then ${screen_off}; else ${sleep}; fi;'';
 }
