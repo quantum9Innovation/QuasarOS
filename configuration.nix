3404e9ba7d8734b9bf9fd15c0c33117be6a43a38
Ananth Venkatesh <dev.quantum9innovation@gmail.com>
2025-08-27 12:41:51 -0700

    fix: cpu perf

--- configuration.nix
+++ configuration.nix
@@ -134,8 +134,8 @@
     tlp = {
       enable = true;
       settings = {
-        CPU_SCALING_GOVERNOR_ON_AC = "powersave";
-        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "performance";
       };
     };
 
